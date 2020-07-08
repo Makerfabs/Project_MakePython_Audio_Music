32d5ac3c1178facd2fe7cf46c0280fab7c31b346
195cn <2259761058@qq.com>
2020-07-08 10:28:17 +0800

    mp3 function ok

--- esp32_mp3/esp32_mp3.ino
+++ esp32_mp3/esp32_mp3.ino
@@ -12,28 +12,42 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+const int Pin_up = 39;
+const int Pin_down = 36;
+const int Pin_pause = 35;
+const int Pin_mute = 34;
+
 const int offset = 0x2C;
 char data[800];
+char null_data[4] = {0, 0, 0, 0};
 long i = 0;
 String music_list[30];
 int music_num = 0;
-int music_index = 1;
+int music_index = 0;
 
 void setup()
 {
+  pinMode(Pin_down, INPUT_PULLUP);
+  pinMode(Pin_up, INPUT_PULLUP);
+  pinMode(Pin_pause, INPUT_PULLUP);
+  pinMode(Pin_mute, OUTPUT);
+  digitalWrite(Pin_mute, LOW);
+
   Serial.begin(115200);
-  Wire.begin(MAKEPYTHON_ESP32_SDA,MAKEPYTHON_ESP32_SCL);
+  Wire.begin(MAKEPYTHON_ESP32_SDA, MAKEPYTHON_ESP32_SCL);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
   display.clearDisplay();
-  testdrawstyles();
+  logoshow();
 
   SPI.begin(18, 19, 23, 5);
   if (!SD.begin(22, SPI))
@@ -42,33 +56,80 @@ void setup()
     return;
   }
   //listDir(SD, "/", 0);
-  music_num = get_wav_list(SD, "/", 0,music_list);
+  music_num = get_wav_list(SD, "/", 0, music_list);
+  Serial.print("Wav count:");
+  Serial.println(music_num);
+
+  music_num--;
   Serial.println("All wav:");
-  for(int i=0;i < music_num;i++)
-  {    
-      Serial.println(music_list[i]);
+  for (int i = 0; i < music_num; i++)
+  {
+    Serial.println(music_list[i]);
   }
 }
 
 void loop()
 {
+  Serial.print("Wav index:");
+  Serial.println(music_index);
   String wav_name = music_list[music_index];
   File file = SD.open(wav_name); // 44100Hz, 16bit, linear PCM
 
   Serial.print(wav_name + "  ");
   Serial.println("OPEN FILE");
-    mydisplay(music_index);
+  mydisplay(music_index);
 
   file.seek(22);
   int ch = file.read();
   file.seek(offset);
-  
+
   I2S_Init();
   Serial.println("I2S");
 
   while (file.readBytes(data, sizeof(data)))
   {
-      I2S_Write(data, sizeof(data));
+
+    if (digitalRead(Pin_down) == 0)
+    {
+      Serial.println("Pin_down");
+      if (music_index < music_num)
+        music_index++;
+      else
+        music_index = 0;
+      delay(100);
+      break;
+    }
+    if (digitalRead(Pin_up) == 0)
+    {
+      Serial.println("Pin_up");
+      if (music_index == 0)
+        music_index = music_num;
+      else
+        music_index--;
+      delay(100);
+      break;
+    }
+    if (digitalRead(Pin_pause) == 0)
+    {
+      Serial.println("Pin_pause");
+      digitalWrite(Pin_mute, HIGH);
+      for (int i = 0; i < sizeof(data); ++i)
+        data[i] = 0; // to prevent buzzing
+      for (int i = 0; i < 5; ++i)
+        I2S_Write(data, sizeof(data));
+      delay(1000);
+      while (1)
+      {
+        if (digitalRead(Pin_pause) == 0)
+        {
+          Serial.println("Pin_pause_play");
+          delay(1000);
+          digitalWrite(Pin_mute, LOW);
+          break;
+        }
+      }
+    }
+    I2S_Write(data, sizeof(data));
   }
   file.close();
   for (int i = 0; i < sizeof(data); ++i)
@@ -77,62 +138,65 @@ void loop()
     I2S_Write(data, sizeof(data));
 
   Serial.println("over");
-  if(music_index < music_num)
-    music_index++;
 }
 
-int get_wav_list(fs::FS &fs, const char * dirname, uint8_t levels,String wavlist[30])
+int get_wav_list(fs::FS &fs, const char *dirname, uint8_t levels, String wavlist[30])
 {
-    Serial.printf("Listing directory: %s\n", dirname);
-    int i = 0;
+  Serial.printf("Listing directory: %s\n", dirname);
+  int i = 0;
 
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("Failed to open directory");
-        return i;
-    }
-    if(!root.isDirectory()){
-        Serial.println("Not a directory");
-        return i;
-    }
-
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            
-        } else {
-            String temp = file.name();
-            if(temp.endsWith(".wav"))
-            {
-                wavlist[i] = temp;
-                i++;
-            } 
-        }
-        file = root.openNextFile();
-    }
+  File root = fs.open(dirname);
+  if (!root)
+  {
+    Serial.println("Failed to open directory");
     return i;
+  }
+  if (!root.isDirectory())
+  {
+    Serial.println("Not a directory");
+    return i;
+  }
+
+  File file = root.openNextFile();
+  while (file)
+  {
+    if (file.isDirectory())
+    {
+    }
+    else
+    {
+      String temp = file.name();
+      if (temp.endsWith(".wav"))
+      {
+        wavlist[i] = temp;
+        i++;
+      }
+    }
+    file = root.openNextFile();
+  }
+  return i;
 }
 
 void mydisplay(int wav_index)
 {
   display.clearDisplay();
 
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
+  display.setTextSize(1);              // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);             // Start at top-left corner
   display.println(music_list[wav_index]);
-    display.display();
+  display.display();
   delay(2000);
 }
 
-
-void testdrawstyles(void) {
+void logoshow(void)
+{
   display.clearDisplay();
 
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Hello, world!"));
-    display.display();
+  display.setTextSize(2);              // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);             // Start at top-left corner
+  display.println(F("Makerfabs"));
+  display.display();
   delay(2000);
 }
